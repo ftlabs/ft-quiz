35b2629c918b5c06b4686fde873dc928a068ec6a
Jamie Brown <jamiebrown201@hotmail.com>
2018-11-13 12:41:54 +0000

    extract out filter

--- handler.js
+++ handler.js
@@ -3,28 +3,25 @@ const articleService = require("./lib/services/articleService");
 const capiService = require("./lib/services/capi");
 const redactedQuestion = require("./lib/questionTypes/redactedHeadline");
 const emojiFace = require("./lib/questionTypes/emojiFace");
-
+const { filterArticles } = require("./lib/services/filter");
 
 module.exports.ftlabsQuiz = async (event, context, callback) => {
   try {
-    const articles = await articleService.get();
-    let articleDetails = await Promise.all(
-      articles.data.topArticleViews.map(article =>
-        capiService.getArticle(article.key)
-      )
-    );
-    // const redactedQuestions = await redactedQuestion.getQuestion(articles);
-    const emojiResults = await emojiFace.getQuestion(articleDetails);
-
-
-
-
+    let articles = await articleService.get();
+    articles = filterArticles(articles.data.topArticleViews);
+    // let articleDetails = await Promise.all(
+    //   articles.data.topArticleViews.map(article =>
+    //     capiService.getArticle(article.key)
+    //   )
+    // );
+    const redactedQuestions = await redactedQuestion.getQuestion(articles);
+    // const emojiResults = await emojiFace.getQuestion(articleDetails);
 
     const response = {
       statusCode: 200,
       headers: { "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify({
-        message: emojiResults,
+        message: redactedQuestions,
         input: event
       })
     };
