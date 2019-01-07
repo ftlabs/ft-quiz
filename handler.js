9cfb01c6c01476b0b85c50233cdebb7049483beb
Jamie Brown <jamiebrown@FT-MW4444.local>
2019-01-07 11:46:22 +0000

    dynamic question adding

--- handler.js
+++ handler.js
@@ -1,11 +1,23 @@
 "use strict";
+const fs = require("fs");
 const articleService = require("./lib/services/articleService");
 const capiService = require("./lib/services/capi");
-const redactedQuestion = require("./lib/questionTypes/redactedHeadline");
-const emojiFace = require("./lib/questionTypes/emojiFace");
+// const redactedQuestion = require("./lib/questionTypes/redactedHeadline");
+// const emojiFace = require("./lib/questionTypes/emojiFace");
 const { filterArticles } = require("./lib/services/filter");
 
+const questions = fs.readdirSync("./lib/questionTypes").map(fileName => {
+  const questionType = fileName.split(".")[0];
+  return {
+    questionType,
+    file: require(`./lib/questionTypes/${questionType}`)
+  };
+});
+
 module.exports.ftlabsQuiz = async (event, context, callback) => {
+  console.log("query", event.queryStringParameters);
+  console.log("questions", questions);
+
   try {
     let articles = await articleService.get();
     articles = filterArticles(articles.data.topArticleViews);
@@ -15,16 +27,24 @@ module.exports.ftlabsQuiz = async (event, context, callback) => {
         return { capiData, lanternData: article };
       })
     );
-    // const redactedQuestions = await redactedQuestion.getQuestion(
+
+    // const redactedQuestions = await questions[0].file.getQuestion(
     //   articleDetails
     // );
-    const emojiResults = await emojiFace.getQuestion(articleDetails);
+
+    // const emojiResults = await emojiFace.getQuestion(articleDetails);
+
+    const results = await Promise.all(
+      questions.map(
+        async question => await question.file.getQuestion(articleDetails)
+      )
+    );
 
     const response = {
       statusCode: 200,
       headers: { "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify({
-        message: emojiResults,
+        message: results,
         input: event
       })
     };
